b4dca8694c52f0513ac5e3c20829a3dfcad503fe
Dmitriy Shulgin <dshulgin@mail.ru>
2024-12-18 15:11:11 +0300

    Main processing loop moved into goroutin

--- cmd/oleninlib/main.go
+++ cmd/oleninlib/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"errors"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 	"github.com/rs/zerolog"
 )
 
 func main() {
 
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	log := zerolog.New(os.Stdout).With().Timestamp().Logger()
+	log := zerolog.New(os.Stdout).With().Str("component", "OLENIN-BOT").Timestamp().Logger()
 
 	err := run(log)
 	if err != nil {
@@ -18,8 +24,85 @@ func main() {
 }
 
 func run(log zerolog.Logger) error {
-	log.Info().Msg("OleninBot started...")
+	log.Info().Msg("запущен")
 
-	log.Info().Msg("OleninBot finished successfully...")
+	var wg sync.WaitGroup
+
+	sigCh := make(chan os.Signal, 1)
+	quitCh := make(chan bool, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	// обработка CTRL+C
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-sigCh
+		quitCh <- true
+		log.Info().Msg("Перехвачен CTRL+C. Завершение работы")
+	}()
+
+	err := godotenv.Load()
+	if err != nil {
+		return err
+	}
+	apiToken, exist := os.LookupEnv("TG_TOKEN")
+	if !exist || len(apiToken) == 0 {
+		return errors.New("TG_TOKEN не определен")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(apiToken)
+	if err != nil {
+		return err
+	}
+
+	_, exist = os.LookupEnv("DEBUG_MODE") //важно наличие, но не значение
+	if exist {
+		bot.Debug = true
+	}
+
+	updateConfig := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	// главный цикл обработки сообщений
+	wg.Add(1)
+	go Update(bot, log, &wg, updates, quitCh)
+
+	wg.Wait()
+
+	log.Info().Msg("завершен успешно")
 	return nil
+}
+
+func Update(
+	bot *tgbotapi.BotAPI,
+	log zerolog.Logger,
+	wg *sync.WaitGroup,
+	updates tgbotapi.UpdatesChannel,
+	quit <-chan bool) {
+
+	defer wg.Done()
+
+	for {
+		select {
+		case quitFlag := <-quit:
+			if quitFlag {
+				log.Info().Msg("завершаю главный цикл")
+				return
+			}
+
+		case update := <-updates:
+			if update.Message == nil {
+				continue
+			}
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "OLB says: "+update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			if _, err := bot.Send(msg); err != nil {
+				log.Error().Err(err).Send()
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module github.com/dgshulgin/oleninlib
 go 1.21.3
 
 require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/rs/zerolog v1.33.0
+)
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rs/zerolog v1.33.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 )
